b176891860eb7194be4a7e12d22ab969087a0c03
EliTu <eliad91@gmail.com>
2019-05-02 21:18:37 +0800

    Setting the state of each square on the Board component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -21,6 +21,13 @@ class Board extends React.Component {
 	state = {
 		squares: Array(9).fill(null),
 	};
+
+	handleClick(i) {
+		const squares = this.state.squares.slice();
+		squares[i] = 'X';
+		this.setState({ squares: squares });
+	}
+
 	renderSquare(i) {
 		return (
 			<Square
